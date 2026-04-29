7830e6b35ceeeb88c6f1b4eba6763c70d74843d2
Cu Thanh Cam <cuthanhcam04@gmail.com>
2026-04-29 14:50:15 +0700

    feat(html-preview): expose module API

---
+++ src/features/html-preview/index.ts
@@ -0,0 +1,7 @@
+export { HtmlPreviewPage } from "./HtmlPreviewPage";
+export {
+  hasHtmlPreviewInput,
+  normalizeHtmlPreviewInput,
+  type HtmlPreviewInput,
+} from "./html-preview.service";
+export { HtmlPreviewSchema, type HtmlPreviewForm } from "./html-preview.schema";
