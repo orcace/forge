1a0514559ae192dcd7573e3d47e406e64677a0a2
Cu Thanh Cam <cuthanhcam04@gmail.com>
2026-05-01 14:05:30 +0700

    feat(diff-checker): expose module API

---
+++ src/features/diff-checker/index.ts
@@ -0,0 +1,7 @@
+export { DiffCheckerPage } from "./DiffCheckerPage";
+export {
+  hasDiffCheckerInput,
+  normalizeDiffCheckerInput,
+  type DiffCheckerInput,
+} from "./diff-checker.service";
+export { DiffCheckerSchema, type DiffCheckerForm } from "./diff-checker.schema";
