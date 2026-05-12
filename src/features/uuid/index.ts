c57fefb83af68618e31b677a5c26cc66ebf9fa93
Cu Thanh Cam <cuthanhcam04@gmail.com>
2026-05-12 16:38:19 +0700

    feat(uuid): expose module API

---
+++ src/features/uuid/index.ts
@@ -0,0 +1,7 @@
+export { UuidPage } from "./UuidPage";
+export {
+  hasUuidInput,
+  normalizeUuidInput,
+  type UuidInput,
+} from "./uuid.service";
+export { UuidSchema, type UuidForm } from "./uuid.schema";
