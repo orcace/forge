7026007c6a2d0537a6650d599ac994601b51885e
Cu Thanh Cam <cuthanhcam04@gmail.com>
2026-05-19 14:02:41 +0700

    feat(slugify): expose module API

---
+++ src/features/slugify/index.ts
@@ -0,0 +1,7 @@
+export { SlugifyPage } from "./SlugifyPage";
+export {
+  hasSlugifyInput,
+  normalizeSlugifyInput,
+  type SlugifyInput,
+} from "./slugify.service";
+export { SlugifySchema, type SlugifyForm } from "./slugify.schema";
