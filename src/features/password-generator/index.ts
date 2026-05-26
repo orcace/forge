bc18ce3903b916165744b3d9dfca6fc5f8eae304
Cu Thanh Cam <cuthanhcam04@gmail.com>
2026-05-26 11:17:17 +0700

    feat(password-generator): expose module API

---
+++ src/features/password-generator/index.ts
@@ -0,0 +1,7 @@
+export { PasswordGeneratorPage } from "./PasswordGeneratorPage";
+export {
+  hasPasswordGeneratorInput,
+  normalizePasswordGeneratorInput,
+  type PasswordGeneratorInput,
+} from "./password-generator.service";
+export { PasswordGeneratorSchema, type PasswordGeneratorForm } from "./password-generator.schema";
